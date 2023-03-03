a6a08bab688cf63b27beb9b7d9b26df9260a453d
fsanchezbz <fsanchezbz@gmail.com>
2023-03-03 16:56:03 +0100

    first commit

---
+++ actions.js
@@ -0,0 +1,19 @@
+console.log("que pasada");
+
+function greet( name,lastne){
+    console.log('Hello'+ name + '' + lastname);
+
+}
+
+let cadenadenombres =["Paco","Pepe"];
+
+let alumnos = {
+  edad:10, 
+  total:10,  
+  nombre:"Paco",
+  
+ };
+
+console.log(alumnos.edad)
+console.log(alumnos.total)
+
